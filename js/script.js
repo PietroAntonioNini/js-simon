9bc6d9fd85d69b3a20f52c5b841afbddd962f817
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-09 16:20:02 +0100

    Creo il contatore

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@
 //salvo in una const l'elemento del timer
 const timerElement = document.querySelector("#time")
 
-// Imposta la data di scadenza per domani alle 9:30
+//imposto la data di scadenza per domani alle 9:30
 
-//data attuale
+//mi salvo la data attuale
 let countDownDate = new Date();
-console.log(countDownDate);
 
 //incremento la data (a domani)
 countDownDate.setDate(countDownDate.getDate() + 1);
@@ -18,3 +17,17 @@ countDownDate.setMinutes(30);
 countDownDate.setSeconds(0);
 
 console.log(countDownDate);
+
+//ora aggiorno il countdown ogni secondo
+setInterval(countDown, 1000);
+
+//creo la funzione countdown
+function countDown() {
+    //data e ora attuali con un timestamp
+    let nowDate = new Date().getTime();
+    console.log(nowDate);
+
+    //distanza tra adesso e la data di scadenza
+    let distance = countDownDate - nowDate;
+    console.log(distance);
+}
